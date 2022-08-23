aec9acd267f8a63e21f25741ab8915e2f7d0f4a4
莫妍 <1312321941@qq.com>
2022-08-23 15:17:39 +0800

    123

--- 微服务架构/WeaveMicro/obj/Debug/net5.0/WeaveMicrocenter.AssemblyInfo.cs
+++ 微服务架构/WeaveMicro/obj/Debug/net5.0/WeaveMicrocenter.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a8991b35-c479-4ab0-ac67-e8206009a1c3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeaveMicrocenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

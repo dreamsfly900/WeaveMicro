bd1fb6c663dfe95c1601ad6d6c4b61b6219ce570
研发部 <@HZZZ>
2023-02-27 23:27:57 +0800

    支持返回类型为IApiResult的文档显示

---
+++ 微服务架构/wRPCService/IApiResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wRPC
+{
+    /// <summary>
+    /// API返回类型
+    /// </summary>
+    public interface IApiResult { }
+}

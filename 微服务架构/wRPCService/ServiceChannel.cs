3baed777e43e1ac1eeadb827669ee78ee82d2eca
葛杰 <xingyu900@live.com>
2023-05-18 19:33:34 +0800

    更新引用

--- 微服务架构/wRPCService/ServiceChannel.cs
+++ 微服务架构/wRPCService/ServiceChannel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.Primitives;
+﻿
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
--- 微服务架构/wRPCService/mode.cs
+++ 微服务架构/wRPCService/mode.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
-using Microsoft.Extensions.Primitives;
+﻿ 
 using System;
 using System.Collections;
 using System.Collections.Generic;
